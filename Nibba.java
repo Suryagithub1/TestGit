157e94a82dcd5254c180a439d597a0e554dbaf34
Suryagithub1 <Suryagithub@gmail.com>
2020-04-07 09:48:24 +0530

    Nibba commit

---
+++ Nibba.java
@@ -0,0 +1,9 @@
+Class Nibba{
+static{
+System.out.println("Dedicated to sum1");
+}
+public static void main(){
+System.out.println("Welcome to Git hub");
+System.out.println("aftr dunatng 5 rups to beggr im feling lik ratan Toyata");  
+}
+}
